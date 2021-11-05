c1f089ab64b9fba7ed24f589ba075a7a23ae45ac
badatt <dev.badatt@gmail.com>
2021-11-06 03:04:10 +0530

    chore: updated template

--- packages/card/src/Card.tsx
+++ packages/card/src/Card.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import cx from "clsx";
 
 export interface ICardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Card = React.forwardRef<HTMLDivElement, ICardProps>((props, ref) => {
   const { className, children, ...rest } = props;
   return (
-    <div ref={ref} {...rest} className={className}>
+    <div ref={ref} {...rest} className={cx(className)}>
       {children}
     </div>
   );
