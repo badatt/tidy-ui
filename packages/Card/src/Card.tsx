45948e89deabf7022e6bcb5e8dce8d093681bcb0
badatt <dev.badatt@gmail.com>
2021-11-05 15:08:21 +0530

    feat: added classname
    BREAKING CHANGE: added classname to card

--- packages/Card/src/Card.tsx
+++ packages/Card/src/Card.tsx
@@ -5,7 +5,7 @@ export interface ICardProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Card = React.forwardRef<HTMLDivElement, ICardProps>((props, ref) => {
   const { className, children, ...rest } = props;
   return (
-    <div ref={ref} {...rest}>
+    <div ref={ref} {...rest} className={className}>
       {children}
     </div>
   );
