0ca5a139019b1431e6099eb9361a6ec80b498359
badatt <dev.badatt@gmail.com>
2021-12-02 23:51:32 +0530

    rebased

--- packages/types/src/index.ts
+++
@@ -1,7 +0,0 @@
-export type Size = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
-
-export type Status = 'info' | 'success' | 'warning' | 'danger';
-
-export type Type = 'primary' | 'secondary' | 'basic' | 'disabled';
-
-export type Appearance = 'filled' | 'outlined' | 'hero';
