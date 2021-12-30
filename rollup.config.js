c3676e42b60d1bf957761c1e386363f45cb2b512
badatt <dev.badatt@gmail.com>
2021-12-31 01:03:56 +0530

    chore: proper builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,12 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
-import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 const packageJson = process.env.PACKAGE_NAME && require(`./packages/${process.env.PACKAGE_NAME}/package.json`);
 
-const dependencies = ({ dependencies }) => Object.keys(dependencies || {});
+const dependencies = ({ peerDependencies }) => Object.keys(peerDependencies || {});
 
 const deps = dependencies(packageJson);
 
@@ -16,20 +15,31 @@ const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: packageJson.main,
-    format: 'es',
-  },
+  output: [
+    {
+      format: 'cjs',
+      file: packageJson.main,
+      banner: '/* eslint-disable */',
+      exports: 'auto',
+    },
+    {
+      format: 'es',
+      file: packageJson.module,
+      banner: '/* eslint-disable */',
+      exports: 'auto',
+    },
+  ],
   plugins: [
     json(),
-    replace({ preventAssignment: true }),
     nodePolyfills(),
-    nodeResolve({ extensions }),
+    nodeResolve({ extensions, preferBuiltins: true }),
     commonjs(),
     terser(),
     typescript({
       inlineSources: true,
       useTsconfigDeclarationDir: true,
+      verbosity: 2,
+      abortOnError: false,
     }),
   ],
   external: (id) => deps.includes(id),
