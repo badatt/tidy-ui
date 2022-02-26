9c0daa346e882962d147e20f26212b09a6b8381f
root <root@EHILP-540.ad.emisgroupplc.com>
2022-02-26 19:13:22 +0530

    feat: badge

---
+++ packages/badge/src/__tests__/Badge.spec.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components';
+import 'jest-styled-components';
+import { orchidLight } from '@tidy-ui/theme';
+import { Badge } from '..';
+
+describe('Badge', () => {
+  test('Default render', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={orchidLight}>
+          <Badge />
+        </ThemeProvider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('dot variant', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={orchidLight}>
+          <Badge variant="dot" />
+        </ThemeProvider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('invisible', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={orchidLight}>
+          <Badge invisible content={10} />
+        </ThemeProvider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('show zero value too', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={orchidLight}>
+          <Badge showZero content={0} />
+        </ThemeProvider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('set a maximum value', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={orchidLight}>
+          <Badge max={9} content={10} />
+        </ThemeProvider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ packages/badge/src/stories/Badge.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { styled } from '@tidy-ui/theme';
+import { EmailIcon } from '@tidy-ui/icons';
+
+import { Badge } from '..';
+
+export default {
+  component: Badge,
+  title: 'Badge',
+};
+
+const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  color: white;
+`;
+
+const Icon = styled(EmailIcon)`
+  height: 20px;
+  width: 20px;
+`;
+
+export const basic = () => (
+  <Container>
+    <Badge content={120} color="success">
+      <Icon />
+    </Badge>
+    <Badge content={120} max={99}>
+      <Icon />
+    </Badge>
+    <Badge content={0}>
+      <Icon />
+    </Badge>
+    <Badge content={0} showZero>
+      <Icon />
+    </Badge>
+    <Badge content={999} invisible>
+      <Icon />
+    </Badge>
+    <Badge variant="dot">
+      <Icon />
+    </Badge>
+  </Container>
+);
