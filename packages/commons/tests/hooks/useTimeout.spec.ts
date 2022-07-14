4934f1935e9ac47d6cb41be53885c7d0f0421972
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-14 20:55:52 +0530

    chore: test for commons

--- packages/commons/tests/hooks/useTimeout.spec.ts
+++ packages/commons/tests/hooks/useTimeout.spec.ts
@@ -2,10 +2,16 @@
  * @jest-environment jsdom
  */
 
-import { renderHook } from '@testing-library/react-hooks/dom';
+import { act, renderHook } from '@testing-library/react-hooks/dom';
 import { useTimeout } from '@tidy-ui/commons';
+import sinon from 'sinon';
 
 describe('useTimeout', () => {
+
+  afterEach(() => {
+    sinon.restore()
+  });
+
   test('Should return clear and reset functions', () => {
     const hook = renderHook(() =>
       useTimeout(() => {
@@ -14,5 +20,45 @@ describe('useTimeout', () => {
     );
     expect(typeof hook.result.current.reset).toEqual('function');
     expect(typeof hook.result.current.clear).toEqual('function');
+  });
+
+  test('Should call passed function after given amount of time', () => {
+    const clock = sinon.useFakeTimers();
+    const cbSpy = sinon.spy();
+    renderHook(() => useTimeout(cbSpy, 30));
+    clock.tick(10);
+    expect(cbSpy.calledOnce).toEqual(false);
+
+    clock.tick(20);
+    expect(cbSpy.calledOnce).toEqual(true);
+  });
+
+  test('Should be able to cancel timeout', () => {
+    const clock = sinon.useFakeTimers();
+    const cbSpy = sinon.spy();
+    const hook = renderHook(() => useTimeout(cbSpy, 30));
+    act(() => {
+      hook.result.current.clear();
+    });
+    clock.tick(10);
+    expect(cbSpy.calledOnce).toEqual(false);
+  });
+
+  test('Should be able to reset timeout', () => {
+    const clock = sinon.useFakeTimers();
+    const cbSpy = sinon.spy();
+    const hook = renderHook(() => useTimeout(cbSpy, 40));
+    act(() => {
+      hook.result.current.clear();
+    });
+    clock.tick(10);
+    expect(cbSpy.calledOnce).toEqual(false);
+
+    act(() => {
+      hook.result.current.reset();
+    });
+
+    clock.tick(40);
+    expect(cbSpy.calledOnce).toEqual(true);
   });
 });
