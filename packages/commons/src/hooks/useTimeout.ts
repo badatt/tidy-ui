ea7b7e09f6d11bb4e12fef5f6362e617f95b03b0
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-14 16:11:38 +0530

    feat: commons init

---
+++ packages/commons/src/hooks/useTimeout.ts
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export interface IUseTimeoutFnReturn {
+  /** Clears the timeout */
+  clear: () => void;
+
+  /** Restarts the timeout. Current timout gets cleared */
+  reset: () => void;
+}
+
+/**
+ * Timeout your callback function
+ * @param fn Timer callback function
+ * @param ms Milliseconds of the timer
+ * @param enabled Whether to open the timer
+ */
+const useTimeout = (fn: (() => void) | undefined, ms = 0, enabled = true): IUseTimeoutFnReturn => {
+  const timeout = useRef<ReturnType<typeof setTimeout>>();
+  const cb = useRef(fn);
+
+  const clearTo = () => {
+    timeout.current && clearTimeout(timeout.current);
+  };
+
+  const clear = useCallback(() => {
+    clearTo();
+  }, []);
+
+  const setTo = useCallback(() => {
+    clearTo();
+    if (enabled) {
+      timeout.current = setTimeout(() => {
+        cb.current?.();
+      }, ms);
+    }
+  }, [ms, enabled]);
+
+  useEffect(() => {
+    cb.current = fn;
+  }, [fn]);
+
+  useEffect(() => {
+    setTo();
+    return clear;
+  }, [ms, enabled, setTo, clear]);
+
+  return { clear, reset: setTo };
+};
+
+export default useTimeout;
