be3e570092476962bc0b4a17b1cf98b0bc48c01f
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-14 17:48:07 +0530

    chore: test for commons

--- packages/commons/tests/hooks/useIsMounted.spec.ts
+++ packages/commons/tests/hooks/useIsMounted.spec.ts
@@ -3,13 +3,12 @@
  */
 
 import { renderHook } from '@testing-library/react-hooks/dom';
-import {useIsMounted} from "@tidy-ui/commons";
+import { useIsMounted } from '@tidy-ui/commons';
 
 describe('useIsMounted', () => {
-
   test('Should return isMounted', () => {
     const hook = renderHook(() => useIsMounted());
-    expect(typeof hook.result.current()).toEqual("boolean");
+    expect(typeof hook.result.current()).toEqual('boolean');
     expect(hook.result.current()).toEqual(true);
   });
 });
