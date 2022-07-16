ed64c604e8fb483eb5528f27ab334a294b4b7ae2
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-16 23:20:33 +0530

    refactor: icons fix

--- packages/icons/src/WarningIcon.tsx
+++ packages/icons/src/WarningIcon.tsx
@@ -10,7 +10,10 @@ export default (props: IIconProps): JSX.Element => (
     viewBox="0 0 24 24"
     fill="currentColor"
   >
-    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+    {props.outlined ? (
+      <path d="M12 5.99 19.53 19H4.47L12 5.99M12 2 1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z" />
+    ) : (
+      <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />
+    )}
   </svg>
 );
