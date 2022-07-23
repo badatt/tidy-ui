d6b055780c85be08c5cba81b80b2dbe749af8e13
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-24 02:40:29 +0530

    chore: lint fix Tag

--- packages/tag/babel.config.js
+++ packages/tag/babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  plugins: ["babel-plugin-styled-components"],
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ],
+  plugins: ['babel-plugin-styled-components'],
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
 };
