9b44095a90f1b85bd9194a874e7426ba226b5b91
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-24 02:39:19 +0530

    feat: implemented Tag and TagGroup

---
+++ packages/icons/src/AddIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IIconProps } from '@tidy-ui/types';
+
+export default (props: IIconProps): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={props.className}
+    focusable={false}
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+  </svg>
+);
