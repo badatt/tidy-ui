84d77be5e1a11a106f6c93150e0ce3d8dc2acef4
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-26 00:30:33 +0530

    chore: reorder theme

--- packages/theme/src/orchid-light.ts
+++ packages/theme/src/orchid-light.ts
@@ -2,6 +2,27 @@ import { ITidyUITheme } from '@tidy-ui/types';
 import { amber, blue, green, neutral, pink, purple, red } from './colors';
 import { createColor, defaultBase } from '.';
 
+export const paletteVariant = {
+  // Red
+  danger: createColor(red),
+  divider: 'hsla(214, 32%, 91%, 1)',
+  // Blue
+  info: createColor(blue),
+  /**
+   * colors taken from tailwind css https://tailwindcss.com/docs/customizing-colors
+   */
+  // Purple
+  major: createColor(purple),
+  // Pink
+  minor: createColor(pink),
+  // Neutral
+  neutral: createColor(neutral),
+  // Green
+  success: createColor(green),
+  // Amber
+  warning: createColor(amber),
+};
+
 export const theme: ITidyUITheme = {
   isDark: false,
   name: 'Orchid Light',
@@ -12,28 +33,11 @@ export const theme: ITidyUITheme = {
       default: 'hsla(216, 56%, 98%, 1)',
       paper: 'hsla(0, 100%, 100%, 1)',
     },
-    // Red
-    danger: createColor(red),
-    divider: 'hsla(214, 32%, 91%, 1)',
-    // Blue
-    info: createColor(blue),
-    /**
-     * colors taken from tailwind css https://tailwindcss.com/docs/customizing-colors
-     */
-    // Purple
-    major: createColor(purple),
-    // Pink
-    minor: createColor(pink),
-    // Neutral
-    neutral: createColor(neutral),
-    // Green
-    success: createColor(green),
+    ...paletteVariant,
     text: {
       disabled: 'hsla(0, 0%, 0%, 0.38)',
       primary: 'hsla(217, 33%, 17%, 1)',
       secondary: 'hsla(0, 0%, 0%, 0.6)',
     },
-    // Amber
-    warning: createColor(amber),
   },
 };
