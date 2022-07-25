e768f564a8e7d01bbd7c495ddad1929551d7c4bf
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-26 00:30:33 +0530

    chore: reorder theme

--- packages/theme/src/orchid-dark.ts
+++ packages/theme/src/orchid-dark.ts
@@ -1,6 +1,6 @@
 import { ITidyUITheme } from '@tidy-ui/types';
-import { amber, blue, green, neutral, pink, purple, red } from './colors';
-import { createColor, defaultBase } from '.';
+import { paletteVariant } from './orchid-light';
+import { defaultBase } from '.';
 
 export const theme: ITidyUITheme = {
   isDark: true,
@@ -12,28 +12,11 @@ export const theme: ITidyUITheme = {
       default: 'hsla(222, 47%, 11%, 1)',
       paper: 'hsla(217, 33%, 17%, 1)',
     },
-    // Red
-    danger: createColor(red),
-    divider: 'hsla(215, 16%, 47%, 1)',
-    // Blue
-    info: createColor(blue),
-    /**
-     * colors taken from tailwind css https://tailwindcss.com/docs/customizing-colors
-     */
-    // Purple
-    major: createColor(purple),
-    // Pink
-    minor: createColor(pink),
-    // Neutral
-    neutral: createColor(neutral),
-    // Green
-    success: createColor(green),
+    ...paletteVariant,
     text: {
       disabled: 'hsla(0, 0%, 100%, 0.5)',
       primary: 'hsla(210, 40%, 96%, 1)',
       secondary: 'hsla(0, 0%, 100%, 0.7)',
     },
-    // Amber
-    warning: createColor(amber),
   },
 };
