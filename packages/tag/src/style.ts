6e93a7bd91891e5ce66baf49332448eddb932058
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-25 19:13:04 +0530

    chore: some lint fixes

--- packages/tag/src/style.ts
+++ packages/tag/src/style.ts
@@ -2,32 +2,32 @@ import { css, hsla } from '@tidy-ui/theme';
 import { ITagGroupProps, ITagProps } from './types';
 
 export const sizeStyles = {
-  sm: {
-    padding: '0.125rem 0.25rem',
-    borderRadius: '0.25rem',
-    fontSize: '.625rem',
-    lineHeight: '.875rem',
-    iconSize: '.625rem',
-    addNewIconSize: '1rem',
-    addNewTagInputWidth: '3rem',
-  },
-  md: {
-    padding: '0.25rem 0.5rem',
-    borderRadius: '0.25rem',
-    fontSize: '.75rem',
-    lineHeight: '1rem',
-    iconSize: '.75rem',
-    addNewIconSize: '1.5rem',
-    addNewTagInputWidth: '4rem',
-  },
   lg: {
-    padding: '0.25rem 0.625rem',
-    borderRadius: '0.25rem',
-    fontSize: '.875rem',
-    lineHeight: '1.25rem',
-    iconSize: '.875rem',
     addNewIconSize: '2rem',
     addNewTagInputWidth: '5rem',
+    borderRadius: '0.25rem',
+    fontSize: '.875rem',
+    iconSize: '.875rem',
+    lineHeight: '1.25rem',
+    padding: '0.25rem 0.625rem',
+  },
+  md: {
+    addNewIconSize: '1.5rem',
+    addNewTagInputWidth: '4rem',
+    borderRadius: '0.25rem',
+    fontSize: '.75rem',
+    iconSize: '.75rem',
+    lineHeight: '1rem',
+    padding: '0.25rem 0.5rem',
+  },
+  sm: {
+    addNewIconSize: '1rem',
+    addNewTagInputWidth: '3rem',
+    borderRadius: '0.25rem',
+    fontSize: '.625rem',
+    iconSize: '.625rem',
+    lineHeight: '.875rem',
+    padding: '0.125rem 0.25rem',
   },
 };
 
--- packages/theme/src/orchid-light.ts
+++ packages/theme/src/orchid-light.ts
@@ -4,16 +4,20 @@ import { amber, blue, green, neutral, pink, purple, red } from './colors';
 import { createColor } from '.';
 
 export const theme: ITidyUITheme = {
-  name: 'Orchid Light',
   isDark: false,
+  name: 'Orchid Light',
   ...defaultBase,
   palette: {
     background: {
       card: 'hsla(0, 100%, 100%, 1)',
-      paper: 'hsla(0, 100%, 100%, 1)',
       default: 'hsla(216, 56%, 98%, 1)',
+      paper: 'hsla(0, 100%, 100%, 1)',
     },
+    // Red
+    danger: createColor(red),
     divider: 'hsla(214, 32%, 91%, 1)',
+    // Blue
+    info: createColor(blue),
     /**
      * colors taken from tailwind css https://tailwindcss.com/docs/customizing-colors
      */
@@ -23,18 +27,14 @@ export const theme: ITidyUITheme = {
     minor: createColor(pink),
     // Neutral
     neutral: createColor(neutral),
-    // Blue
-    info: createColor(blue),
     // Green
     success: createColor(green),
-    // Amber
-    warning: createColor(amber),
-    // Red
-    danger: createColor(red),
     text: {
+      disabled: 'hsla(0, 0%, 0%, 0.38)',
       primary: 'hsla(217, 33%, 17%, 1)',
       secondary: 'hsla(0, 0%, 0%, 0.6)',
-      disabled: 'hsla(0, 0%, 0%, 0.38)',
     },
+    // Amber
+    warning: createColor(amber),
   },
 };
