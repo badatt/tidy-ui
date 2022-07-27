bd5ef11122e2cc661644ad894c3e0b2536ed86ff
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-28 02:00:16 +0530

    fix: badge component prop changes and updates

--- packages/badge/tests/Badge.spec.tsx
+++ packages/badge/tests/Badge.spec.tsx
@@ -1,69 +1,127 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
+import '@testing-library/jest-dom';
 import 'jest-styled-components';
-import { orchidLight } from '@tidy-ui/theme';
+import { DangerousIcon } from '@tidy-ui/commons';
+import { orchidLight, styled } from '@tidy-ui/theme';
 import { Badge } from '../src';
 
+const Icon = styled(DangerousIcon)`
+  height: 20px;
+  width: 20px;
+`;
+
 describe('Badge', () => {
-  test('Default render', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Basic render', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
-  test('Major color', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge color="major" />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Badges with all color variants', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge color="major">
+          <Icon />
+        </Badge>
+        <Badge color="minor">
+          <Icon />
+        </Badge>
+        <Badge color="neutral">
+          <Icon />
+        </Badge>
+        <Badge color="info">
+          <Icon />
+        </Badge>
+        <Badge color="success">
+          <Icon />
+        </Badge>
+        <Badge color="warning">
+          <Icon />
+        </Badge>
+        <Badge color="danger">
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
-  test('dot variant', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge variant="dot" />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Badge without max value should show 99+', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge withData={198}>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
-  test('invisible', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge invisible content={10} />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Badge with max value', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge withData={198} withMax={150}>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
-  test('show zero value too', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge showZero content={0} />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Badge with a dot', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge isDotted>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
-  test('set a maximum value', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Badge max={9} content={10} />
-        </ThemeProvider>,
-      )
-      .toJSON();
+  test('Badge with a dot and it blinks', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge isDotted isBlink>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  test('Badge which is invisible', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge isInvisible withData={50}>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  test('Do not show zero value by default', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge withData={0}>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  test('Force show zero value', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Badge isShowZero withData={0}>
+          <Icon />
+        </Badge>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
