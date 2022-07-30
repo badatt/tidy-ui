ba0c08b0f18e950c6a40c0ce85c23f5b8b6657a6
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-30 14:06:03 +0530

    fix: changes to Anchor button component

---
+++ packages/button/stories/Anchor.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { styled } from '@tidy-ui/theme';
+import { Anchor } from '../src';
+
+export default {
+  component: Anchor,
+  title: 'Anchor',
+};
+
+const Container = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+export const basic = () => (
+  <Container>
+    <Anchor href="/?path=/story/anchor--basic">Basic</Anchor>
+  </Container>
+);
+
+export const disabled = () => (
+  <Container>
+    <Anchor href="/?path=/story/anchor--basic" isDisabled>
+      disabled
+    </Anchor>
+  </Container>
+);
+
+export const launchNewTab = () => (
+  <Container>
+    <Anchor href="https://www.google.com/" isLaunch>
+      Google
+    </Anchor>
+  </Container>
+);
