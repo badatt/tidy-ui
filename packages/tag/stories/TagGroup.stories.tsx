ce7660db6ebcc2bf6b28f3aea729a7d80761cef5
Balu Praveen Datty <dev.badatt@gmail.com>
2022-07-30 14:53:13 +0530

    docs: tags stories

--- packages/tag/stories/TagGroup.stories.tsx
+++ packages/tag/stories/TagGroup.stories.tsx
@@ -14,54 +14,6 @@ const Container = styled.div`
   gap: 1rem;
 `;
 
-export const basic = () => (
-  <TagGroup>
-    <Tag magnitude="sm">html</Tag>
-    <Tag>typescript</Tag>
-    <Tag magnitude="lg">javascript</Tag>
-    <Tag disable>cloud</Tag>
-    <Tag icon={<CheckCircleIcon />}>kotlin</Tag>
-  </TagGroup>
-);
-
-export const colors = () => (
-  <TagGroup>
-    <Tag color="major">html</Tag>
-    <Tag color="minor">typescript</Tag>
-    <Tag color="neutral">cloud</Tag>
-    <Tag color="success">javascript</Tag>
-    <Tag color="info">java</Tag>
-    <Tag color="warning">sass</Tag>
-    <Tag color="danger">css</Tag>
-  </TagGroup>
-);
-
-export const outlined = () => (
-  <TagGroup>
-    <Tag color="major" outlined>
-      html
-    </Tag>
-    <Tag color="minor" outlined>
-      typescript
-    </Tag>
-    <Tag color="neutral" outlined>
-      cloud
-    </Tag>
-    <Tag color="success" outlined>
-      javascript
-    </Tag>
-    <Tag color="info" outlined>
-      java
-    </Tag>
-    <Tag color="warning" outlined>
-      sass
-    </Tag>
-    <Tag color="danger" outlined>
-      css
-    </Tag>
-  </TagGroup>
-);
-
 export const appendable = () => {
   const [tags, setTags] = useState(['react', 'java', 'cloud']);
 
