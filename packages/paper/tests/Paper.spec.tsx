f0148ab015e0741a0d06782c38223bc9a3ab940f
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-01 00:52:49 +0530

    chore: paper fix

--- packages/paper/tests/Paper.spec.tsx
+++ packages/paper/tests/Paper.spec.tsx
@@ -1,42 +1,57 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
+import '@testing-library/jest-dom';
 import 'jest-styled-components';
-import { orchidLight } from '@tidy-ui/theme';
+import { orchidDark, orchidLight } from '@tidy-ui/theme';
 import { Paper } from '../src';
 
 describe('Paper', () => {
-  it('Default render', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Paper>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam ducimus ea reiciendis sed blanditiis
-            impedit quisquam velit praesentium, eius dignissimos. Omnis ad doloribus laudantium expedita accusantium
-            placeat suscipit dolores. Autem iste voluptas deleniti similique accusamus voluptatem dolorem necessitatibus
-            aut natus facere, tenetur sequi culpa beatae praesentium hic aliquam velit molestias illo enim sint
-            repellendus earum quasi est repudiandae. Eveniet?
-          </Paper>
-        </ThemeProvider>,
-      )
-      .toJSON();
+  it('Basic render', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Paper>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam ducimus ea reiciendis sed blanditiis
+          impedit quisquam velit praesentium, eius dignissimos. Omnis ad doloribus laudantium expedita accusantium
+          placeat suscipit dolores. Autem iste voluptas deleniti similique accusamus voluptatem dolorem necessitatibus
+          aut natus facere, tenetur sequi culpa beatae praesentium hic aliquam velit molestias illo enim sint
+          repellendus earum quasi est repudiandae. Eveniet?
+        </Paper>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
 
-  it('Rounded paper', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={orchidLight}>
-          <Paper rounded>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam ducimus ea reiciendis sed blanditiis
-            impedit quisquam velit praesentium, eius dignissimos. Omnis ad doloribus laudantium expedita accusantium
-            placeat suscipit dolores. Autem iste voluptas deleniti similique accusamus voluptatem dolorem necessitatibus
-            aut natus facere, tenetur sequi culpa beatae praesentium hic aliquam velit molestias illo enim sint
-            repellendus earum quasi est repudiandae. Eveniet?
-          </Paper>
-        </ThemeProvider>,
-      )
-      .toJSON();
+  it('Dark mode basic render', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidDark}>
+        <Paper>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam ducimus ea reiciendis sed blanditiis
+          impedit quisquam velit praesentium, eius dignissimos. Omnis ad doloribus laudantium expedita accusantium
+          placeat suscipit dolores. Autem iste voluptas deleniti similique accusamus voluptatem dolorem necessitatibus
+          aut natus facere, tenetur sequi culpa beatae praesentium hic aliquam velit molestias illo enim sint
+          repellendus earum quasi est repudiandae. Eveniet?
+        </Paper>
+      </ThemeProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Paper with sharp edges', () => {
+    const tree = render(
+      <ThemeProvider theme={orchidLight}>
+        <Paper isSharp>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam ducimus ea reiciendis sed blanditiis
+          impedit quisquam velit praesentium, eius dignissimos. Omnis ad doloribus laudantium expedita accusantium
+          placeat suscipit dolores. Autem iste voluptas deleniti similique accusamus voluptatem dolorem necessitatibus
+          aut natus facere, tenetur sequi culpa beatae praesentium hic aliquam velit molestias illo enim sint
+          repellendus earum quasi est repudiandae. Eveniet?
+        </Paper>
+      </ThemeProvider>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
