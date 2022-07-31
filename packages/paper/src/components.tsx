6c47f62305515cfa23447d7487a55a24523016b2
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-01 00:52:49 +0530

    chore: paper fix

---
+++ packages/paper/src/components.tsx
@@ -0,0 +1,20 @@
+import { css, styled } from '@tidy-ui/theme';
+import { IPaperProps } from './types';
+
+/**
+ * Paper root component
+ *
+ */
+const PaperRoot = styled.section<IPaperProps>`
+  ${({ theme, isSharp }) => css`
+    display: flex;
+    padding: 1rem;
+    background-color: ${theme.palette.background.paper};
+    border-radius: ${!isSharp && theme.layout.radius};
+    box-shadow: ${theme.layout.shadow};
+    color: ${theme.palette.text.primary};
+    margin-bottom: 1.875rem;
+  `}
+`;
+
+export { PaperRoot };
--- packages/paper/src/types.ts
+++ packages/paper/src/types.ts
@@ -1,5 +1,19 @@
 import { HTMLAttributes } from 'react';
 
+/**
+ * Paper props
+ *
+ * @export
+ * @interface IPaperProps
+ * @typedef {IPaperProps}
+ * @extends {HTMLAttributes<HTMLDivElement>}
+ */
 export interface IPaperProps extends HTMLAttributes<HTMLDivElement> {
-  rounded?: boolean;
+  /**
+   * If `true`, the component is created with sharp edges
+   *
+   * @default false
+   * @type {?boolean}
+   */
+  isSharp?: boolean;
 }
