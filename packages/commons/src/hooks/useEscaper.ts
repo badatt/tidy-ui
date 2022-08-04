8e94a868085dfa3965ac4d2af1993b597e9a058e
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-05 02:21:50 +0530

    chore: disable sonar one regex

--- packages/commons/src/hooks/useEscaper.ts
+++ packages/commons/src/hooks/useEscaper.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 
 const useEscaper = (inp?: string) => {
-  return useMemo(() => inp?.replace(/[\W]+/g, '-'), [inp]);
+  return useMemo(() => inp?.replace(/[\W]+/g, '-'), [inp]); //NOSONAR
 };
 
 export default useEscaper;
