54d26363c65907bb0ba91483617d2e70440e4fad
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-06 16:10:45 +0530

    chore: lint fix

--- packages/panel/src/types.ts
+++ packages/panel/src/types.ts
@@ -9,9 +9,9 @@ export interface IPanelProps extends HTMLAttributes<HTMLDivElement> {
   isExpanded?: boolean;
 }
 
-export interface IPanelHeaderProps extends HTMLAttributes<HTMLDivElement> {}
+export type IPanelHeaderProps = HTMLAttributes<HTMLDivElement>
 export interface IPanelBodyProps extends HTMLAttributes<HTMLDivElement> {
   isVisible?: boolean;
 }
 
-export interface IPanelGroupProps extends HTMLAttributes<HTMLDivElement> {}
+export type IPanelGroupProps = HTMLAttributes<HTMLDivElement>
