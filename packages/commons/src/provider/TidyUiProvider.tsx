5d1b24174120cf1c7e28929c5b84c23a08775854
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-09 22:39:47 +0530

    feat: added TidyUiProvider for theming and context

---
+++ packages/commons/src/provider/TidyUiProvider.tsx
@@ -0,0 +1,61 @@
+import React, { createContext, Key, ReactNode, useMemo } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { ITidyUITheme } from '@tidy-ui/types';
+import usePortal from '../hooks/usePortal';
+
+/**
+ *
+ */
+export interface ITidyUiProviderProps {
+  /**
+   *
+   */
+  children?: ReactNode;
+
+  /**
+   *
+   */
+  key?: Key;
+  /**
+   *
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ref?: any;
+  /**
+   *
+   */
+  theme?: ITidyUITheme;
+}
+
+const TidyUiContext = createContext<ITidyUiProviderProps>({});
+
+const { Consumer, Provider } = TidyUiContext;
+
+/**
+ * Custom TidyUiProvider
+ *
+ * @param {ITidyUiProviderProps} props TidyUiProvider props
+ * @returns {ReactNode} react node
+ */
+const TidyUiProvider = (props: ITidyUiProviderProps): ReactNode => {
+  const { children, theme, ...rest } = props;
+
+  const { Portal } = usePortal();
+
+  console.log('Calling custom provider');
+
+  const value = useMemo(() => ({ theme, ...rest }), [theme, rest]);
+
+  return (
+    <ThemeProvider theme={theme} {...rest}>
+      <Provider value={value}>
+        {children}
+        <Portal>
+          <div>portal</div>
+        </Portal>
+      </Provider>
+    </ThemeProvider>
+  );
+};
+
+export { Consumer as TidyUiConsumer, TidyUiContext, TidyUiProvider };
