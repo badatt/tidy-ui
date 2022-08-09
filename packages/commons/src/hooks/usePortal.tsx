ae8b8853b18057d7a7b88549b722848f9fcd30d1
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-09 22:38:39 +0530

    feat: added usePortal

---
+++ packages/commons/src/hooks/usePortal.tsx
@@ -0,0 +1,79 @@
+import { FC, ReactNode, useCallback, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import isValidDom from '../utils/isValidDom';
+
+/**
+ * Portal props
+ */
+export interface IPortalProps {
+  /**
+   * Container to launch portal on
+   */
+  container?: HTMLElement | (() => HTMLElement | null) | null;
+  /**
+   * Id of the parent element
+   */
+  id?: string;
+}
+
+/**
+ * usePortal return
+ */
+export interface IUsePortalFnReturn {
+  /**
+   * Portal component
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Portal: FC<{
+    /**
+     * Node children
+     */
+    children: ReactNode;
+  }>;
+  /**
+   * Root target element
+   */
+  target: HTMLElement | null;
+}
+
+/**
+ * Hook to create Portal
+ *
+ * @param {IPortalProps?} props hook props
+ * @returns {IUsePortalFnReturn} portal and its parent element target
+ */
+const usePortal = (props?: IPortalProps): IUsePortalFnReturn => {
+  const rootRef = useRef<HTMLElement | null>(isValidDom ? document.body : null);
+
+  useEffect(() => {
+    const containerEle = typeof props?.container === 'function' ? props.container() : props?.container;
+    const existingParent = props?.id && document.getElementById(props.id);
+    const parent = containerEle || existingParent || document.body;
+    rootRef.current = parent;
+  }, [rootRef, props?.container, props?.id]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const Portal: FC<{
+    /**
+     * Node children
+     */
+    children: ReactNode;
+  }> = useCallback(
+    ({
+      children,
+    }: {
+      /**
+       * Node children
+       */
+      children: ReactNode;
+    }) => {
+      if (rootRef.current != null) return createPortal(children, rootRef.current);
+      return null;
+    },
+    [rootRef],
+  );
+
+  return { Portal, target: rootRef.current };
+};
+
+export default usePortal;
--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -14,11 +14,16 @@ export { default as RotatingCircleIcon } from './icons/RotatingCircleIcon';
 export { default as WarningIcon } from './icons/WarningIcon';
 
 /** utils */
+export { default as isValidDom } from './utils/isValidDom';
 
 /** hooks */
 export { default as useEscaper } from './hooks/useEscaper';
 export { default as useHash } from './hooks/useHash';
 export { default as useIsMounted } from './hooks/useIsMounted';
+export { default as usePortal } from './hooks/usePortal';
 export { default as useTheme } from './hooks/useTheme';
 export { default as useTimeout } from './hooks/useTimeout';
 export { default as useWindow } from './hooks/useWindow';
+
+/** others */
+export * from './provider/TidyUiProvider';
