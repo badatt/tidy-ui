d06ce0850e424214be629233169b8e221c97a076
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-15 18:46:05 +0530

    chore: release minor for divider component

--- packages/divider/src/Divider.tsx
+++ packages/divider/src/Divider.tsx
@@ -12,6 +12,10 @@ const DividerRoot = styled.hr<IDividerProps>`
   `}
 `;
 
+/**
+ * Divider can be use to give a horizontal separation between components, a good replacement
+ * for <hr> in html, powered by various styles (with theming) and settings
+ */
 const Divider = forwardRef<HTMLHRElement, IDividerProps>((props, ref) => {
   const { className, ...rest } = props;
   return <DividerRoot ref={ref} className={className} role="divider" {...rest} />;
