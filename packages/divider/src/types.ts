dae7e66c008b0a4568776545a6d424aca66be815
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-15 18:46:05 +0530

    chore: release minor for divider component

--- packages/divider/src/types.ts
+++ packages/divider/src/types.ts
@@ -2,24 +2,34 @@ import { HTMLAttributes } from 'react';
 import { TColor, TShade } from '@tidy-ui/types';
 
 /**
- *
+ * Divider props
  */
 export interface IDividerProps extends HTMLAttributes<HTMLHRElement> {
   /**
+   * The color of the divider
    *
+   * @type {?TColor}
+   * @default 'neutral'
    */
   withColor?: TColor;
   /**
+   * Thickness of the divider
    *
+   * @default 'thin'
    */
   withDensity?: 'thin' | 'medium' | 'thick';
   /**
+   * Shade of the color of the divider, 50 being low, 900 being high
    *
+   * @type {?TShade}
+   * @default 500
    */
   withShade?: TShade;
 
   /**
+   * Divider style
    *
+   * @default 'solid'
    */
   withStyle?: 'dashed' | 'dotted' | 'double' | 'groove' | 'hidden' | 'ridge' | 'solid';
 }
