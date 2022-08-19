f4502b3c3f95f0e27b79b1d85793a009755061fd
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-20 01:22:39 +0530

    chore: base props

--- packages/types/src/common.ts
+++ packages/types/src/common.ts
@@ -1,4 +1,40 @@
-import { HTMLAttributes } from 'react';
+import { AriaRole, CSSProperties, ElementType, HTMLAttributes, ReactNode } from 'react';
+
+/**
+ * Base props for Tidy UI components
+ */
+export interface IBaseProps {
+  /**
+   * Children
+   */
+  children?: ReactNode;
+  /**
+   * Additional classes
+   */
+  className?: string;
+  /**
+   * Unique identifier
+   */
+  id?: string;
+  /**
+   * Aria role
+   */
+  role?: AriaRole;
+  /**
+   * Inline styles
+   */
+  style?: CSSProperties;
+}
+
+/**
+ * Props with `as`
+ */
+export interface IAsProps<A extends ElementType | string = ElementType> extends IBaseProps {
+  /**
+   * `as` a html tag component
+   */
+  as?: A;
+}
 
 /**
  * Common props for Icon components
