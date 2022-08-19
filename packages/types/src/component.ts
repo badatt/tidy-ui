a8534bcf0fc79813c53ed4c8a2434c7658e21aa4
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-20 01:22:39 +0530

    chore: base props

---
+++ packages/types/src/component.ts
@@ -0,0 +1,24 @@
+import { ComponentPropsWithRef, ElementType, PropsWithChildren, ReactElement } from 'react';
+import { IAsProps } from './common';
+
+export type Pick<T, K extends keyof T> = { [P in K]: T[P] };
+export type Exclude<T, U> = T extends U ? never : T;
+export type Omit<T, U> = Pick<T, Exclude<keyof T, keyof U>>;
+
+export type ReplaceProps<Inner extends ElementType, P> = Omit<ComponentPropsWithRef<Inner>, P> & P;
+
+/**
+ * Ref forwarding component
+ */
+export interface IRefForwardingComponent<T extends ElementType, P = unknown> {
+  <As extends React.ElementType = T>(props: PropsWithChildren<ReplaceProps<As, IAsProps<As> & P>>): ReactElement | null;
+
+  /**
+   * Display name
+   */
+  displayName?: string;
+  /**
+   * prop types
+   */
+  propTypes?: Record<string, unknown>;
+}
