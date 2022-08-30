16689b88c2a69dd18ab4f6793e4fcb3ccc1845c6
Balu Praveen Datty <dev.badatt@gmail.com>
2022-08-30 10:52:40 +0530

    feat: added vertical support for divider component

--- packages/divider/src/types.ts
+++ packages/divider/src/types.ts
@@ -2,6 +2,16 @@ import { HTMLAttributes } from 'react';
 import { IBaseProps, TShade, TTone } from '@tidy-ui/types';
 
 /**
+ * Divider style props
+ */
+export type TVariant = 'dashed' | 'dotted' | 'double' | 'groove' | 'hidden' | 'ridge' | 'solid';
+
+/**
+ * Divider density props
+ */
+export type TDensity = 'thin' | 'medium' | 'thick';
+
+/**
  * Divider props
  */
 export interface IDividerProps extends IBaseProps, HTMLAttributes<HTMLHRElement> {
@@ -10,9 +20,9 @@ export interface IDividerProps extends IBaseProps, HTMLAttributes<HTMLHRElement>
    *
    * @default 'thin'
    */
-  density?: 'thin' | 'medium' | 'thick';
+  density?: TDensity;
   /**
-   * Top and bottom margins
+   * Margins
    */
   margin?: string;
   /**
@@ -35,10 +45,56 @@ export interface IDividerProps extends IBaseProps, HTMLAttributes<HTMLHRElement>
    *
    * @default 'solid'
    */
-  variant?: 'dashed' | 'dotted' | 'double' | 'groove' | 'hidden' | 'ridge' | 'solid';
+  variant?: TVariant;
 
   /**
    * If `true`, set the divider vertically
+   *
+   * @default false
    */
   vertical?: boolean;
+}
+
+/**
+ * DividerEnhanced props
+ */
+export interface IEnhancedDividerProps extends IBaseProps, HTMLAttributes<HTMLDivElement> {
+  /**
+   * Thickness of the divider
+   *
+   * @default 'thin'
+   */
+  density?: TDensity;
+  /**
+   * css `justify-content` equivalent
+   *
+   * @default 'center'
+   */
+  justify?: 'center' | 'start' | 'end';
+
+  /**
+   * Margins
+   */
+  margin?: string;
+
+  /**
+   * Shade of the color of the divider, 50 being low, 900 being high
+   *
+   * @type {?TShade}
+   * @default 500
+   */
+  shade?: TShade;
+  /**
+   * The color of the divider
+   *
+   * @type {?TTone}
+   * @default 'neutral'
+   */
+  tone?: TTone;
+  /**
+   * Divider variant
+   *
+   * @default 'solid'
+   */
+  variant?: TVariant;
 }
--- packages/divider/stories/Divider.stories.tsx
+++ packages/divider/stories/Divider.stories.tsx
@@ -5,7 +5,7 @@ import { Divider } from '../src';
 
 export default {
   component: Divider,
-  title: 'Layout/Divider',
+  title: 'Layout/Divider/Divider',
 };
 
 const Container = styled.div`
@@ -27,7 +27,7 @@ export const basic = () => (
         veritatis recusandae magni accusamus rem quo cupiditate architecto hic culpa eum totam reprehenderit, libero
         veniam!
       </Text>
-      <Divider density="thick" />
+      <Divider />
       <Text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, id vero quibusdam aperiam officia
         veritatis recusandae magni accusamus rem quo cupiditate architecto hic culpa eum totam reprehenderit, libero
@@ -54,13 +54,49 @@ export const basic = () => (
 export const variants = () => (
   <Container>
     <Column>
-      <Text>color major</Text>
+      <Text v="subtitle2">major tone</Text>
+      <Text>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur officiis excepturi ea distinctio praesentium
+        commodi cumque totam, reiciendis eveniet magni quis asperiores rem quos maiores perferendis eligendi? Accusamus,
+        deleniti doloremque. Neque magnam quis fuga nobis laboriosam debitis itaque minima praesentium voluptatibus
+        vero. Eligendi illum aut laboriosam assumenda voluptatem nam veritatis repellendus id velit consectetur soluta
+        voluptatum, repellat hic obcaecati quibusdam! Enim fugit doloribus dolore, laudantium ratione praesentium
+        laboriosam vero nobis fuga rem culpa perspiciatis ea accusamus voluptas aut. Facilis obcaecati fugiat sapiente
+        dolorem. Ab voluptas, eaque doloribus esse suscipit iste.
+      </Text>
       <Divider tone="major" />
-      <Text>thick density</Text>
+      <Text v="subtitle2">thick density</Text>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis totam maxime ullam impedit fuga incidunt!
+        Dolore perspiciatis quidem expedita. Enim, quaerat mollitia numquam itaque vitae obcaecati porro sed laboriosam
+        quam. In, autem est necessitatibus cum dolorum enim. Numquam iure rerum dolore perspiciatis harum impedit
+        pariatur maiores placeat reprehenderit odio tenetur nulla nam, labore id blanditiis consequatur debitis
+        veritatis quis expedita. Beatae nam fugiat quia est enim doloremque nostrum nisi nobis asperiores nulla, sit
+        labore voluptas rerum laboriosam at provident explicabo dicta non eaque perspiciatis cupiditate qui possimus.
+        Reprehenderit, illum quas?
+      </Text>
       <Divider density="thick" />
-      <Text>800 shade</Text>
+      <Text v="subtitle2">800 shade</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae corporis ratione et culpa, provident
+        dolorem atque in consectetur officiis inventore dolore quos assumenda! Voluptates perferendis provident
+        voluptate commodi id. Cum nisi perspiciatis maiores facere beatae temporibus sit omnis quod nesciunt quia
+        consequuntur quis quam voluptate earum ducimus rem, aliquam necessitatibus explicabo eligendi ut distinctio
+        dicta non. Modi, vero optio. Sunt ipsam mollitia totam, eum accusamus aut ut, suscipit repellendus quaerat error
+        nihil quasi blanditiis architecto accusantium numquam alias maiores officia non. Tempora odio ducimus temporibus
+        labore reiciendis animi eveniet!
+      </Text>
       <Divider shade={800} />
-      <Text>dashed style</Text>
+      <Text v="subtitle2">dashed style</Text>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non ut omnis nobis dolores deleniti voluptatum
+        sapiente molestias ex minus. Aperiam itaque exercitationem voluptate nulla quas quo earum illum cupiditate
+        sapiente. Ducimus asperiores iusto tenetur omnis quam esse, aperiam sunt odio laborum neque quaerat fuga,
+        molestias nesciunt qui maiores hic veniam vero quod rem. Ipsam enim alias, eaque minima ipsum doloribus. Impedit
+        velit deleniti expedita omnis voluptatibus consectetur repellendus doloremque aliquam, deserunt dolore repellat
+        consequatur accusantium aut! Placeat fuga, ipsa molestiae dolorum, facere excepturi rerum quas consectetur nemo
+        ab enim architecto?
+      </Text>
       <Divider variant="dashed" />
     </Column>
   </Container>
