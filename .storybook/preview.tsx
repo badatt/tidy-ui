91ed640ab2301d8a7b5cdb61c77f6fc42439a919
Balu Praveen Datty <dev.badatt@gmail.com>
2022-09-06 16:10:32 +0530

    chore: removed custom font

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -73,10 +73,7 @@ export const withTheme: DecoratorFn = (StoryFn, context) => {
       return (
         <>
           <GlobalReset />
-          <GlobalFont
-            family="PT Sans"
-            url="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          />
+          <GlobalFont />
           <TidyUiProvider theme={storyTheme}>
             <GlobalDefault />
             <ThemeBlock filled padding={padding}>
