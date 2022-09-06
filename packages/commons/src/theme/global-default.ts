9877643974838047e9575ce2eac8ef12c7b49e2e
Balu Praveen Datty <dev.badatt@gmail.com>
2022-09-06 16:09:41 +0530

    feat: global reset & font

---
+++ packages/commons/src/theme/global-default.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+import { IGlobalDefault } from '@tidy-ui/types';
+import { css } from '.';
+
+const GlobalDefault = createGlobalStyle<IGlobalDefault>`
+  ${({ theme: { palette } }) => css`
+    html {
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      font-size: 16px;
+    }
+
+    body {
+      color: ${palette.text.primary};
+      min-height: 100vh;
+      height: 100%;
+      letter-spacing: 0.025rem;
+      line-height: 1;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-rendering: optimizeLegibility;
+    }
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    button {
+      appearance: none;
+      background-color: transparent;
+      cursor: pointer;
+      display: inline-block;
+      line-height: 1;
+    }
+  `}
+`;
+
+export { GlobalDefault };
