6bb245c42bad041c20a906f24385fb0fafc7febe
Balu Praveen Datty <dev.badatt@gmail.com>
2022-09-13 00:31:04 +0530

    chore: reset toast

--- packages/toaster/src/Toaster.tsx
+++ packages/toaster/src/Toaster.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { styled, TidyUiContext, usePortal } from '@tidy-ui/commons';
+import { styled, usePortal } from '@tidy-ui/commons';
 import { IToast, IToasterProps } from './types';
 
 const ToasterRoot = styled.div`
