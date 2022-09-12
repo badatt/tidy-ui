be9054278965c07e1b9c1a7be15f26d4961229dc
Balu Praveen Datty <dev.badatt@gmail.com>
2022-09-13 00:30:22 +0530

    chore: reset toaster

--- packages/toaster/stories/Toaster.stories.tsx
+++ packages/toaster/stories/Toaster.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Actions, css, hsla, color, styled, TidyUiContext } from '@tidy-ui/commons';
 import { Button, ButtonGroup, IconButton } from '@tidy-ui/button';
-import { Toaster, useToaster } from '../src';
+import { Toaster } from '../src';
 
 const ToastContent = styled.div`
   ${({ theme: { palette, layout } }) => css`
@@ -18,33 +18,15 @@ export default {
   title: 'Feedback/Toaster',
 };
 
-export const dummy = () => {
-  const { dispatch } = React.useContext(TidyUiContext);
-
-  const addToast = () => {
-    dispatch({
-      type: Actions.Toaster.AddToast,
-      payload: {
-        id: Math.random().toString(),
-        item: 'Toast',
-      },
-    });
-  };
-
-  return <Button onClick={addToast}>Add Toast</Button>;
-};
-
 export const withToaster = () => {
-  const { toaster } = useToaster();
-
   const ele = <ToastContent>{Date.now()}</ToastContent>;
 
   return (
     <div>
       <ButtonGroup>
-        <Button onClick={() => toaster.push(ele)}>Add</Button>
-        <Button onClick={() => toaster.pop()}>Pop</Button>
-        <Button onClick={() => toaster.clear()}>Clear</Button>
+        <Button onClick={() => null}>Add</Button>
+        <Button onClick={() => null}>Pop</Button>
+        <Button onClick={() => null}>Clear</Button>
       </ButtonGroup>
       <Toaster timeout="infinite" />
     </div>
