74e810104526524c6cadb05b8dfdd3c364b2c1bc
Balu Praveen Datty <dev.badatt@gmail.com>
2022-09-16 16:09:51 +0530

    feat: toned icon component

---
+++ packages/commons/src/icons/TonedIcon.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { TTone } from '@tidy-ui/types';
+import { Icon } from './Icon';
+
+/**
+ * Toned icons
+ *
+ * @template T
+ * @param {props} props for toned icon
+ * @returns {JSX.Element} icon
+ */
+export default <
+  T extends {
+    /**
+     * Tone of the icon
+     */
+    tone?: TTone;
+  },
+>(
+  props: T,
+): JSX.Element => {
+  switch (props.tone) {
+    case 'info':
+      return <Icon.Info />;
+    case 'success':
+      return <Icon.CheckCircle />;
+    case 'warning':
+      return <Icon.Warning />;
+    case 'danger':
+      return <Icon.Dangerous />;
+    default:
+      return <></>;
+  }
+};
