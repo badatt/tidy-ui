a6d211237fc775bffa0669a34e24a3993adf641a
badatt <dev.badatt@gmail.com>
2023-05-05 23:27:44 +0530

    chore: release minor for smell fix in Checkbox

--- packages/checkbox/src/Checkbox.tsx
+++ packages/checkbox/src/Checkbox.tsx
@@ -8,10 +8,15 @@ import { ICheckboxProps } from './types';
  */
 const Checkbox = React.forwardRef<HTMLInputElement, ICheckboxProps>((props, ref) => {
   const { children, checked, indeterminate, ...rest } = props;
+
+  const isChecked = React.useMemo(() => {
+    return [checked, indeterminate].some(Boolean);
+  }, [checked, indeterminate]);
+
   return (
     <CheckboxRoot ref={ref} role="checkbox" {...rest}>
       <HiddenCheckboxInput type="checkbox" {...rest} />
-      <StyledCheckboxInput checked={checked || indeterminate} {...rest}>
+      <StyledCheckboxInput checked={isChecked} {...rest}>
         <TickIcon viewBox="0 0 24 24">
           {indeterminate ? <path d="M19 13H5v-2h14v2z" /> : <polyline points="20 6 9 17 4 12" />}
         </TickIcon>
