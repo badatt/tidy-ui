c1949d1af9f4bef7758b6ae33dae4ecccdb167b8
badatt <dev.badatt@gmail.com>
2023-05-17 09:18:27 +0530

    test: update

--- packages/container/tests/Container.spec.tsx
+++ packages/container/tests/Container.spec.tsx
@@ -75,4 +75,136 @@ describe('Container', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+
+  it('Custom background', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container background="grey">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom border', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container border="2px solid grey">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom clear', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container clear="both">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom color', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container color="grey">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom display', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container display="block">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom float', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container float="left">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom font', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container font="Roboto">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom margin', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container margin="25rem">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom overflow', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container overflow="scroll">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom padding', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container padding="25rem">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom position', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container position="absolute">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Custom width', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Container width="25rem">
+          <Content />
+        </Container>
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
