b1f3e27b5ab4a3292543e2a7f6e66d82611a5f24
badatt <dev.badatt@gmail.com>
2023-05-23 13:57:48 +0530

    chore: props enhancements for Container

--- packages/container/src/Container.tsx
+++ packages/container/src/Container.tsx
@@ -1,59 +1,16 @@
 import React from 'react';
-import { applyStandardOverrideStyles, css, devices, styled } from '@tidy-ui/commons';
+import { ContainerRoot } from './components';
 import { IContainerProps } from './types';
-
-const ContainerRoot = styled.div<IContainerProps>`
-  ${({
-    theme: {
-      layout: { screens },
-    },
-    isFixed,
-    maxWidth,
-    gutter,
-  }) => css`
-    margin-left: auto;
-    margin-right: auto;
-    box-sizing: border-box;
-    ${maxWidth &&
-    css`
-      max-width: ${screens[maxWidth].breakpoint}px;
-    `}
-    ${gutter &&
-    css`
-      padding: 0 ${gutter};
-    `}
-    ${isFixed &&
-    css`
-      ${devices.mobile} {
-        max-width: ${screens.xs.breakpoint}px;
-      }
-      ${devices.tablet} {
-        max-width: ${screens.sm.breakpoint}px;
-      }
-      ${devices.laptop} {
-        max-width: ${screens.md.breakpoint}px;
-      }
-      ${devices.desktop} {
-        max-width: ${screens.lg.breakpoint}px;
-      }
-      ${devices.tv} {
-        max-width: ${screens.xl.breakpoint}px;
-      }
-    `}
-  `}
-  ${applyStandardOverrideStyles}
-`;
 
 /**
  * Container is a wrapped div element that can responsively contain enclosed elements.
  * This can be usually used as page root component
  */
 const Container = React.forwardRef<HTMLDivElement, IContainerProps>((props, ref) => {
-  const { children, ...rest } = props;
-  console.log(rest);
+  const { children, ele, ...rest } = props;
   return (
     <ContainerRoot ref={ref} role="main" {...rest}>
-      {children}
+      {ele ? React.cloneElement(ele, {}, children) : children}
     </ContainerRoot>
   );
 });
