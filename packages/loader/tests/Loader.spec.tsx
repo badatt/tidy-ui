291f5ea4ffb8b6b2b0807b2c694bfb20ae5cedeb
badatt <dev.badatt@gmail.com>
2023-07-14 12:34:12 +0530

    chore: release minor for Loader component

--- packages/loader/tests/Loader.spec.tsx
+++ packages/loader/tests/Loader.spec.tsx
@@ -5,14 +5,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
-import { orchidDark, orchidLight, TidyUiProvider } from '../../commons/src';
+import { Girth, orchidDark, orchidLight, TidyUiProvider } from '../../commons/src';
 import { Loader } from '../src';
 
 describe('Loader', () => {
   it('Basic render', () => {
     const tree = render(
       <TidyUiProvider theme={orchidLight}>
-        <Loader>basic</Loader>
+        <Loader />
       </TidyUiProvider>,
     );
     expect(tree).toMatchSnapshot();
@@ -20,7 +20,33 @@ describe('Loader', () => {
   it('Dark mode basic render', () => {
     const tree = render(
       <TidyUiProvider theme={orchidDark}>
-        <Loader>basic</Loader>
+        <Loader />
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('Variants', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        <Loader />
+        <Loader variant="ellipses" />
+        <Loader variant="pulse" />
+      </TidyUiProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('Girths', () => {
+    const tree = render(
+      <TidyUiProvider theme={orchidLight}>
+        {Object.keys(Girth)
+          .filter((i) => !isNaN(Number(i)))
+          .map((v) => (
+            <div key={v}>
+              <Loader girth={Girth[v]} />
+              <Loader girth={Girth[v]} variant="ellipses" />
+              <Loader girth={Girth[v]} variant="pulse" />
+            </div>
+          ))}
       </TidyUiProvider>,
     );
     expect(tree).toMatchSnapshot();
