aa2ea120be33d4ea7f116fa551c6ad443488edcb
badatt <dev.badatt@gmail.com>
2023-08-05 22:08:30 +0530

    chore: fix types

--- packages/tag/src/types.ts
+++ packages/tag/src/types.ts
@@ -70,7 +70,7 @@ export interface ITagGroupProps extends IBaseProps, HTMLAttributes<HTMLDivElemen
   /**
    * On add callback
    *
-   * @type {?() => void}
+   * @type {function}
    */
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   onAddNewTag?: (v: any) => void;
